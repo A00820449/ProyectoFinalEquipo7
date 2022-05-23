763ce163548cd5191e7318a109339ab289a31e60
Miguel Tornero <a00820449@itesm.mx>
2022-05-22 23:38:35 -0500

    added recycler view

---
+++ app/src/main/java/com/example/proyectofinalequipo7/RecyclerViewAdapter.kt
@@ -0,0 +1,24 @@
+package com.example.proyectofinalequipo7
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.proyectofinalequipo7.databinding.TodoItemBinding
+
+class RecyclerViewAdapter(var todos : List<Todo>) : RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+    class ViewHolder(val binding: TodoItemBinding) : RecyclerView.ViewHolder(binding.root) {}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val binding = TodoItemBinding.inflate(layoutInflater,parent,false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
