c90e767037c59b91423a1a46e54d020efc04e786
amsuarez99 <amsuarez99@gmail.com>
2022-06-14 13:09:21 -0500

    feat: settings screen

--- app/src/main/java/com/example/proyectofinalequipo7/SettingsFragment.kt
+++ app/src/main/java/com/example/proyectofinalequipo7/SettingsFragment.kt
@@ -1,10 +1,23 @@
 package com.example.proyectofinalequipo7
 
+import android.app.Activity
+import android.content.Intent
+import android.content.res.Resources
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContentProviderCompat.requireContext
+import androidx.core.content.ContextCompat.startActivity
+import androidx.fragment.app.Fragment
+import com.example.proyectofinalequipo7.databinding.FragmentSettingsBinding
+import java.util.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -20,13 +33,23 @@ class SettingsFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    private var _binding : FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+    private var currentLanguage = "en"
+    private var currentLang: String? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
+
+        currentLanguage = requireActivity().intent.getStringExtra(currentLang).toString()
+
+
     }
 
     override fun onCreateView(
@@ -34,7 +57,74 @@ class SettingsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        // Inflate the layout for this fragment
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        handleThemeChange()
+        setupSpinner()
+        return binding.root
+    }
+
+    private fun handleThemeChange() {
+        binding.switch1.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            }
+        }
+    }
+
+    private fun setupSpinner() {
+        val languages = resources.getStringArray(R.array.languages_array)
+
+        // access the spinner
+        val spinner = binding.spinner
+        if (spinner != null) {
+            val adapter = ArrayAdapter.createFromResource(
+                requireContext(),
+                R.array.languages_array,
+                android.R.layout.simple_spinner_item
+            ).also { adapter ->
+                // Specify the layout to use when the list of choices appears
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                // Apply the adapter to the spinner
+                spinner.adapter = adapter
+            }
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(parent: AdapterView<*>,
+                                            view: View?, position: Int, id: Long) {
+                    when (position) {
+                        0 -> {}
+                        1 -> setLocale("en")
+                        2 -> setLocale("es")
+                    }
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+    }
+
+    private fun setLocale(localeName: String) {
+        Log.i("help", localeName)
+        if(localeName != currentLanguage) {
+            val locale = Locale(localeName)
+            val res = resources
+            val dm = res.displayMetrics
+            val conf = res.configuration
+            conf.locale = locale
+            res.updateConfiguration(conf, dm)
+            val refresh = Intent(
+                requireContext(),
+                MainActivity::class.java
+            )
+            refresh.putExtra(currentLang, localeName)
+            startActivity(refresh)
+        }
     }
 
     companion object {
